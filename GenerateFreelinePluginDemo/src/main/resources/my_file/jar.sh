797cf87ca6c030b1b8cf79374a2bf0752d2439b5
wangqing <wangqing@kaolafm.com>
2016-09-16 23:29:32 +0800

    1. 将 sh 命令 修改变量名。

--- GenerateFreelinePluginDemo/src/main/resources/my_file/jar.sh
+++ GenerateFreelinePluginDemo/src/main/resources/my_file/jar.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "当前 sh 的命令路径是:" $0
-a=$0
-b=${a%/*}
-echo "自动跳转到 " $b
-cd $b
+filePath=$0
+parentPath=${filePath%/*}
+echo "自动跳转到 " $parentPath
+cd $parentPath
 # pwd
 # java -jar GenerateFreelinePluginDemo-1.0-SNAPSHOT.jar
 
-b=$(ls *.jar)
-echo "查找到 jar 文件：" $b
-java -jar $b
+parentPath=$(ls *.jar)
+echo "查找到 jar 文件：" $parentPath
+java -jar $parentPath
