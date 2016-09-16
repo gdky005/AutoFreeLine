135aead345749fc554e6eeac7c82d480f66779f1
wangqing <wangqing@kaolafm.com>
2016-09-16 12:19:32 +0800

    初始化 git 项目完成。

---
+++ GenerateFreelinePluginDemo/src/main/groovy/Test.groovy
@@ -0,0 +1,44 @@
+/**
+ * Created by WangQing on 16/9/16.
+ */
+println "开始获取配置文件"
+
+
+/** 默认情况下是 false, 当打成 jar 之后就设置为 true */
+def isRunJar = true
+def path = System.getProperty("user.dir")
+
+def configFile = "my.properties"
+InputStream inputStream;
+
+File tempFile = new File("test.test")
+
+if (tempFile.exists())
+    tempFile.delete()
+
+if (!tempFile.exists()) {
+    tempFile.createNewFile()
+    String parentPath = tempFile.getAbsolutePath()
+
+    parentPath = parentPath.substring(0, parentPath.indexOf(tempFile.getName()))
+
+    println "临时文件的地址" + parentPath
+    tempFile.delete()
+}
+
+
+println "当前运行文件的路径是：" + path
+println ""
+println ""
+println "输出结果是："
+
+if (isRunJar) {
+    String proFilePath = System.getProperty("user.dir") + "/" + configFile;
+    inputStream = new BufferedInputStream(new FileInputStream(proFilePath));
+} else {
+    inputStream = getClass().getResourceAsStream(configFile)
+}
+
+ResourceBundle property = new PropertyResourceBundle(inputStream);
+String name = property.getString("name");
+println name
